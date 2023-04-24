600b2a5e08a383863183b4fca62c5be29c64eb8b
Hugo Jacquel <h.jacquel@eleve.leschartreux.net>
2023-04-24 08:15:04 +0200

    Personal Project ON

--- src/components/page/PersonalProject.vue
+++ src/components/page/PersonalProject.vue
@@ -13,6 +13,9 @@
     />
 
 
+
+
+
     <Project 
         ProjectName="RES-Appli"
         ProjectDescription="Application (client lourd/léger Electron JS) de <b>gestion de réservations en ligne </b> selon des contraintes 
@@ -26,6 +29,63 @@
         ProjectDate="(Janvier - Avril 2023)"
         ProjectTech="Figma , HTML, CSS, JS, Electron JS, Node JS (avec intégration SQL possible)"
     />
+
+
+    <Project
+    ProjectName="Libft"
+    ProjectDescription="Réimplémentation des fonctions de la librairie standard en c, il s'agit d'un projet de l'école 42.
+    <br> Le projet est réalisé en C et doit être optimisé au maximum et ne doit pas avoir de fuite de mémoire."
+    ProjectLink="https://github.com/MrHugojuggernot/Libft"
+    ProjectImg="42.jpg"
+    ProjectDate="(Septembre 2022)"
+    ProjectTech="C"
+    />
+
+
+
+    <Project
+    ProjectName="BSQ (Biggest SQuare)"
+    ProjectDescription="BSQ est un projet à faire en 24h top chrono en groupe de 2 (Projet final de piscine 42). L'objectif est de trouver le plus grande carrée de caractère dans un fichier passé en paramètre ou dans le stdin
+    <br> Le projet est réalisé en C et doit être optimisé au maximum et ne doit pas avoir de fuite de mémoire. Pour ce projet, on utilise du parsing et du lexing ainsi que du backtracking pour la vérification du carée."
+    ProjectLink="https://github.com/MrHugojuggernot/BSQ"
+    ProjectImg="42.jpg"
+    ProjectDate="(Aout 2022 (24h))"
+    ProjectTech="C"
+    />
+
+
+    <Project
+    ProjectName="Piscine 42 en C et Shell"
+    ProjectDescription="La piscine est un marathon de programmation sur un mois, il s'agit de l'examen d'entrée pour l'école 42
+    <br> La piscine est composé de plusieurs mini-projet pour étudier les notions en algorithmies,en structure de données ainsi que les notions avancées en programmation (pointeur , pointeur de fonction par exemple) en C et en Shell"
+    ProjectLink="https://github.com/MrHugojuggernot/C-Pool"
+    ProjectImg="42.jpg"
+    ProjectDate="(Aout 2022)"
+    ProjectTech="C, Shell"
+    />
+
+    <Project
+    ProjectName="CalC"
+    ProjectDescription="Calculatrice en C en ligne de commande, utilisation du parsing et du lexing pour la gestion des opérations et des priorités."
+    ProjectLink="https://github.com/MrHugojuggernot/CalC"
+    ProjectImg="CalC.jpg"
+    ProjectDate="(Juillet 2022)"
+    ProjectTech="C"
+    />
+
+
+    <Project
+    ProjectName="Task Manager | Discord Bot"
+    ProjectDescription="Bot discord permettant de gérer des tâches.
+    <br> Les tâches sont sérialisés sur un fichier de données sur un serveur pour être conservé même si le bot est redémarré.
+    <br> L'objectif de ce projet était d'apprendre le parsing de donnée grâce à la librairie Discord.py et Discord.js 
+    <br> et de pouvoir sérialiser en utilisant JSON et numpy."
+    ProjectLink=""
+    ProjectImg="discord-js.png"
+    ProjectDate="(Avant 2020)"
+    ProjectTech="Python, Discord.py Numpy , Javascript , Discord JS, JSON"
+    />
+
 
 </template>
 
